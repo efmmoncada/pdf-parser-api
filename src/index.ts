2bac180394e6f1fdf054de7027f4fd18d60c7ba5
Moncada <moncadae@hsd.k12.or.us>
2025-02-01 00:59:31 -0800

    init commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+type File = string;
+
+app.post('/readText', (req: Request, res: Response) => {
+  const { file }: {file: File} = req.body;
+  console.log(file);
+
+  res.status(200).send('File received');
+});
+
+app.listen(process.env.PORT ?? 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+}); 
